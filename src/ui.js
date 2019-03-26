648980d914373a779daeff9b7994a2b87409eea6
Jarod Peachey <jwpeachey107@aol.com>
2019-03-25 23:03:09 -0400

    Add UI module

---
+++ src/ui.js
@@ -0,0 +1,17 @@
+class UI {
+   constructor() {
+      this.posts = document.querySelector('#posts');
+      this.titleInput = document.querySelector('#title');
+      this.bodyInput = document.querySelector('#body');
+      this.idInput = document.querySelector('#id');
+      this.postSubmit = document.querySelector('.post-submit');
+      this.forState = 'add';
+   }
+
+   showPosts(posts) {
+      console.log('This is from the UI file');
+      console.log(posts);
+   }
+}
+
+export const ui = new UI();
