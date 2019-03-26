6a96aa5007e955eed77d95baaec11fdf25d77ecf
Jarod Peachey <jwpeachey107@aol.com>
2019-03-25 23:37:41 -0400

    Add posts display

--- src/ui.js
+++ src/ui.js
@@ -9,8 +9,25 @@ class UI {
    }
 
    showPosts(posts) {
-      console.log('This is from the UI file');
-      console.log(posts);
+      let output = '';
+
+      posts.forEach((post) => {
+         output += `
+         <div class="card">
+            <div class="card-body">
+               <div>
+                  <h2 class="mb-0 display-inline-block">${post.title}</h2>
+                  <a href="#" class="delete float-right" data-id="${post.id}"><i class="fa fa-trash-alt"></i></a>
+                  <a href="#" class="edit float-right pr-2" data-id="${post.id}"><i class="fa fa-pencil-alt"></i></a>
+               </div>
+               <small class="subtitle mt-0 pt-0">Posted on 12/12/26</small>
+               <p>${post.body}</p>
+
+            </div>
+         </div>
+         `
+         this.posts.innerHTML = output;
+      })
    }
 }
 
