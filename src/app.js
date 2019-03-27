64a5b6102fde6436c3b2187e23087153c49e20f8
Jarod Peachey <jwpeachey107@aol.com>
2019-03-26 21:05:24 -0400

    Add post functionality

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Add a posts
+ui.postSubmit.addEventListener('click', submitPost);
+
+// Get posts
 function getPosts() {
    http.get('http://localhost:8082/posts')
    .then(data => ui.showPosts(data))
    .catch(err => console.log(err));
+}
+
+function submitPost() {
+   let title = ui.titleInput.value;
+   let body = ui.bodyInput.value;
+
+   // Set new post object
+   const data = {
+      title,
+      body
+   }  
+   
+   // Create post in API
+   http.post('http://localhost:8082/posts', data)
+      .then(data => {
+         getPosts();
+      })
+      .catch(err => console.log(err))
 }
